31fc4b91863b20116e2d72bf1186f03933601dc2
Robert Schmieder <robert.schmieder87@gmx.de>
2022-12-06 14:28:52 +0100

    Update UnionFighterController.cs
    - reorganized 'Steering-Mode'-Code (different ways to move the player ship around) for better readability
    - disabled the possibility to change the steering mode by pressing a key -> will be set from Level-Settings in the future

--- Assets/Scripts/PlayerScripts/UnionFighterController.cs
+++ Assets/Scripts/PlayerScripts/UnionFighterController.cs
@@ -27,6 +27,9 @@ public class UnionFighterController : MonoBehaviour
     [SerializeField]
     private float hoverMinPos = -6.55f;
 
+    private bool lastWasUp = true;
+    private float hoverThrust = 500f;
+
     private float levelBorderX = 17f;
     private float levelBorderY = 8.5f;
 
@@ -39,7 +42,6 @@ public class UnionFighterController : MonoBehaviour
     {
         FighterRB = GetComponent<Rigidbody2D>();
         steeringMode = FighterSteeringMode.DefaultHoverX;
-        MovementInput.y = YAcceleration;
         TheGame.theGameInst.onMissionCanBegin += Enable_Controls;
         TheGame.theGameInst.onLevelLoad += Reset_FighterPosition;
         ControlsEnabled = true;
@@ -49,90 +51,117 @@ public class UnionFighterController : MonoBehaviour
     void Update()
     {
         if (ControlsEnabled)
-        {
-            if(Input.GetKeyDown(KeyCode.M))
-            {
-                steeringMode++;                              
-
-                int steeringModeIndex = (int)steeringMode;
-                int steeringModeLength = Enum.GetNames(typeof(FighterSteeringMode)).Length;
-                if (steeringModeIndex > steeringModeLength - 1)
-                    steeringMode = FighterSteeringMode.DefaultHoverX;
-
-                Reset_FighterPosition();
-                FighterRB.freezeRotation = true;
-            }
-
+        {   
+            
             InputX = Input.GetAxisRaw("Horizontal");
             InputY = Input.GetAxisRaw("Vertical");
 
-            switch (steeringMode) 
-            {
-                case FighterSteeringMode.DefaultHoverX:
-                    MovementInput.x = InputX;
-                    MovementInput.y = YAcceleration;
-                    break;
-
-                case FighterSteeringMode.FreeMovementXY:                    
-                    MovementInput.x = InputX;
-                    MovementInput.y = InputY;
-                    break;
-
-                case FighterSteeringMode.HoverY:
-                    MovementInput.x = InputX;
-                    MovementInput.y = InputY;
-                    FighterRB.rotation = -90f;
-                    break;
-
-                case FighterSteeringMode.Rotate:
-                    FighterRB.freezeRotation = false;                                
-                    break;
-            }
+            ControlMovement_AccordingTo_SteeringMode();            
         }        
     }
 
     private void FixedUpdate()
+    {        
+        MoveUnionFighter();
+    }
+
+    private void Set_SteeringMode_PressingKey()
     {
-        if(steeringMode == FighterSteeringMode.DefaultHoverX)
-        {            
-            Vector2 NextFighterPos = FighterRB.position + MovementInput * movementSpeed * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            steeringMode++;
 
-            if (NextFighterPos.x < levelBorderX && NextFighterPos.x > -(levelBorderX))
-            {
-                if (NextFighterPos.y >= hoverMaxPos || NextFighterPos.y <= hoverMinPos)
-                    SwitchYDirection();
+            int steeringModeIndex = (int)steeringMode;
+            int steeringModeLength = Enum.GetNames(typeof(FighterSteeringMode)).Length;
+            if (steeringModeIndex > steeringModeLength - 1)
+                steeringMode = FighterSteeringMode.DefaultHoverX;
 
-                FighterRB.MovePosition(NextFighterPos);                
+            Reset_FighterPosition();
+            FighterRB.freezeRotation = true;
+        }
+    }
 
-                Check_YPosition();
-            }
+    private void ControlMovement_AccordingTo_SteeringMode()
+    {
+        switch (steeringMode)
+        {
+            case FighterSteeringMode.DefaultHoverX:
+                MovementInput.x = InputX;
+                MovementInput.y = YAcceleration;
+                break;
+
+            case FighterSteeringMode.FreeMovementXY:
+                MovementInput.x = InputX;
+                MovementInput.y = InputY;
+                break;
+
+            case FighterSteeringMode.HoverY:
+                MovementInput.x = InputX;
+                MovementInput.y = InputY;
+                FighterRB.rotation = -90f;
+                break;
+
+            case FighterSteeringMode.Rotate:
+                FighterRB.freezeRotation = false;
+                break;
+        }
+    }
+
+    private void MoveUnionFighter()
+    {
+        if (steeringMode == FighterSteeringMode.DefaultHoverX)
+        {
+            MoveDefaultHoverX();
         }
 
-        else if(steeringMode == FighterSteeringMode.FreeMovementXY || steeringMode == FighterSteeringMode.HoverY)
-        {   
-            Vector2 NextFighterPos = FighterRB.position + MovementInput * movementSpeed * Time.deltaTime;
-
-            if (NextFighterPos.x < levelBorderX && NextFighterPos.x > -(levelBorderX) && NextFighterPos.y < levelBorderY && NextFighterPos.y > -(levelBorderY))
-                FighterRB.MovePosition(NextFighterPos);
-
+        else if (steeringMode == FighterSteeringMode.FreeMovementXY || steeringMode == FighterSteeringMode.HoverY)
+        {
+            MoveFreelyXY();
         }
 
-        else if(steeringMode == FighterSteeringMode.Rotate)
-        {            
-            float NextRotation;
-
-            if (InputX != 0 && InputY == 0)
-                NextRotation = FighterRB.rotation - InputX * rotationSpeed * Time.deltaTime;
-
-            else if (InputX == 0 && InputY != 0)
-                NextRotation = FighterRB.rotation - InputY * rotationSpeed * Time.deltaTime;
-
-            else
-                NextRotation = FighterRB.rotation - InputX * rotationSpeed * Time.deltaTime;
-
-            FighterRB.MoveRotation(NextRotation);
+        else if (steeringMode == FighterSteeringMode.Rotate)
+        {
+            MoveRotating();
         }
+    }
 
+    private void MoveDefaultHoverX()
+    {
+        Vector2 NextFighterPos = FighterRB.position + MovementInput * movementSpeed * Time.deltaTime;
+
+        if (NextFighterPos.x < levelBorderX && NextFighterPos.x > -(levelBorderX))
+        {
+            if (NextFighterPos.y >= hoverMaxPos || NextFighterPos.y <= hoverMinPos)
+                SwitchYDirection();
+
+            FighterRB.MovePosition(NextFighterPos);
+
+            Check_YPosition();
+        }
+    }
+
+    private void MoveFreelyXY()
+    {
+        Vector2 NextFighterPos = FighterRB.position + MovementInput * movementSpeed * Time.deltaTime;
+
+        if (NextFighterPos.x < levelBorderX && NextFighterPos.x > -(levelBorderX) && NextFighterPos.y < levelBorderY && NextFighterPos.y > -(levelBorderY))
+            FighterRB.MovePosition(NextFighterPos);
+    }
+
+    private void MoveRotating()
+    {
+        float NextRotation;
+
+        if (InputX != 0 && InputY == 0)
+            NextRotation = FighterRB.rotation - InputX * rotationSpeed * Time.deltaTime;
+
+        else if (InputX == 0 && InputY != 0)
+            NextRotation = FighterRB.rotation - InputY * rotationSpeed * Time.deltaTime;
+
+        else
+            NextRotation = FighterRB.rotation - InputX * rotationSpeed * Time.deltaTime;
+
+        FighterRB.MoveRotation(NextRotation);
     }
 
     private void Enable_Controls()
@@ -176,6 +205,29 @@ public class UnionFighterController : MonoBehaviour
     private void SwitchYDirection()
     {
         MovementInput.y *= -1f;
+    }
+
+    private void SwitchImpulseDirection()
+    {
+        hoverThrust *= -1f;
+    }
+
+    private void Hover()
+    {
+
+        if (lastWasUp)
+        {
+            SwitchImpulseDirection();
+            lastWasUp = false;
+        }
+
+        else
+        {
+            SwitchImpulseDirection();
+            lastWasUp = true;
+        }
+
+        FighterRB.AddForce(Vector2.up * hoverThrust, ForceMode2D.Impulse);
     }
 
     private enum FighterSteeringMode
