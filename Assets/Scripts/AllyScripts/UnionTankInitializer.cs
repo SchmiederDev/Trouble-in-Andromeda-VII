a3373572ceff4952d25a3d9bd233bb89f32d4c24
Robert Schmieder <robert.schmieder87@gmx.de>
2022-12-06 13:55:06 +0100

    Create UnionTankInitializer.cs
    Just a container class for basic settings for an NPC-Ship, fetched and set when an object of this type is instantiated

---
+++ Assets/Scripts/AllyScripts/UnionTankInitializer.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnionTankInitializer : MonoBehaviour
+{    
+    public float rotationSpeed = 2.5f;
+    public float attackRange = 2f;
+    public List<string> targetPriorities;
+}
