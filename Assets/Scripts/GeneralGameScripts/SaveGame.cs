52e06c636f462112b8bed05db94e723aa60cd460
Robert Schmieder <robert.schmieder87@gmx.de>
2023-01-04 13:44:58 +0100

    Transfer unlocked weapons method
    Property and Method for SaveGame-Class (Data Container) to store/ save unlocked weapons accomplishments in SaveGame-File and reload them when game is started anew

--- Assets/Scripts/GeneralGameScripts/SaveGame.cs
+++ Assets/Scripts/GeneralGameScripts/SaveGame.cs
@@ -5,10 +5,21 @@ public class SaveGame
 {
     public int TotalXP;
     public int LevelsPlayed;
+    public bool[] unlockedWeapons;
 
     public SaveGame(int totalXP, int levelsPlayed)
     {
         TotalXP = totalXP;
         LevelsPlayed = levelsPlayed;
     }   
+
+    public void TransferUnlockedWeapons()
+    {
+        unlockedWeapons = new bool[TheGame.theGameInst.PlayerUnionFighter.FighterWeapons.Count];
+
+        for(int i = 0; i < unlockedWeapons.Length; i++)
+        {
+            unlockedWeapons[i] = TheGame.theGameInst.PlayerUnionFighter.FighterWeapons[i].IsActivated;
+        }
+    }
 }
